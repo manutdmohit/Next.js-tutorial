9c023935273e8d3c98140e904c069ad566aeaaf8
manutdmohit <saudmohit@gmail.com>
2021-10-17 23:27:54 +0545

    getStaticProps and getStaticPaths

--- pages/blog/[pageNo].js
+++ pages/blog/[pageNo].js
@@ -2,7 +2,36 @@ import { useRouter } from 'next/router';
 import Navbar from '../../components/Navbar';
 import Head from 'next/head';
 
-const pageNo = () => {
+export const getStaticPaths = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await res.json();
+
+  const paths = data.map((currElem) => {
+    return {
+      params: {
+        pageNo: currElem.id.toString(),
+      },
+    };
+  });
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps = async (context) => {
+  const id = context.params.pageNo;
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+  const data = await res.json();
+  return {
+    props: {
+      data,
+    },
+  };
+};
+
+const pageNo = ({ data }) => {
   const router = useRouter();
   const pageNumber = router.query.pageNo;
   return (
@@ -11,7 +40,11 @@ const pageNo = () => {
         <title>Blog {pageNumber}</title>
       </Head>
       <Navbar />
-      Blog {pageNumber}
+      <div className="ssr-styles ssr-styles-inside">
+        <h3>{data.id}</h3>
+        <h2>{data.title}</h2>
+        <p>{data.body}</p>
+      </div>
     </>
   );
 };
--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,17 +1,33 @@
 import Navbar from '../../components/Navbar';
 import Head from 'next/head';
+import Link from 'next/link';
 
+export const getStaticProps = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await res.json();
+  return {
+    props: {
+      data,
+    },
+  };
+};
 
-const blog = () => {
+const blog = ({ data }) => {
   return (
     <>
-    <Head>
-      <title>Blog</title>
-    </Head>
+      <Head>
+        <title>Blog</title>
+      </Head>
       <Navbar />
-      This is blog page
+      {data.slice(0, 5).map((currElem) => (
+        <div key={currElem.id} className="ssr-styles">
+          <h3>{currElem.id}</h3>
 
-      
+          <Link href={`/blog/${currElem.id}`}>
+            <h2>{currElem.title}</h2>
+          </Link>
+        </div>
+      ))}
     </>
   );
 };
